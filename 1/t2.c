f11a8469c9a196341a7e1874a205ee69571ea717
ttt13 <ttt13@sfu.ca>
2017-05-15 20:32:05 -0700

    t2 submission

---
+++ 1/t2.c
@@ -0,0 +1,8 @@
+#include <limits.h>
+#include <stdio.h>
+
+int main( void )
+{
+	printf("%lu %lu %lu %lu %lu %lu %lu %lu %lu\n ", sizeof(char)*8, sizeof(unsigned char)*8, sizeof(int)*8, sizeof(unsigned int)*8, sizeof(long)*8, sizeof(unsigned long)*8, sizeof(float)*8, sizeof(double)*8, sizeof(long double)*8  );
+	return 0;
+}
