f85d29b937abfc4e08e72e5ef0ac6af1a08b591e
ttt13 <ttt13@sfu.ca>
2017-05-19 22:45:55 -0700

    t9 submission

---
+++ 1/t9.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+
+int main( void )
+{
+	float x = 0; // Width (base value)
+	float y = 0; // Depth (height value)
+	float z = 0; // Length **Irrelevant**
+	float a = 0; // Counter to evaluate left and right
+	float b = 0; // Counter to print hashtags
+	char hashtag = '#';
+	char dot = '.';
+	float left = 0; // For left '#' placement location
+	float right = 0; // For right '#' placement location
+
+	scanf("%f %f %f", &x, &z, &y);
+
+	for ( a = (y - 1); a >= 0; a-- ){
+		left = floor( a * ( x / (2 * y) ) );
+		right = ceil( (x-1) + (-a) * ( x/(2 * y) ) );
+
+		for ( b = (x - 1); b >= 0; b-- ){
+			if( a == (y-1) || a == 0 ){
+				if ( b < left || b > right ){
+					printf(" ");
+				}
+				else{
+				printf("%c", hashtag);
+				}
+			}
+			else if( a < y || a > 0 ){
+				if( b < left || b > right ){
+					printf(" ");
+				}
+				else if( b == left || b == right){
+					printf("%c", hashtag);
+				}
+				else{
+					printf("%c", dot);
+				}
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
