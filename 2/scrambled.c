e1ae129f26fc49a03d9057479645cb7953d91b69
ttt13 <ttt13@sfu.ca>
2017-05-23 22:32:26 -0700

    scrambled is working

--- 2/scrambled.c
+++ 2/scrambled.c
@@ -3,6 +3,7 @@
 int scrambled( unsigned int a[], unsigned int b[], unsigned int len )
 {
 	unsigned int temp = 0;
+	int counter = 0;
 	if (len == 0){
 		return 1;
 	}else{
@@ -10,10 +11,29 @@ int scrambled( unsigned int a[], unsigned int b[], unsigned int len )
 			temp = a[i];
 			for (int n = 0; n < len; n++){
 				if (b[n] == temp){
-					return 1;
+					counter += 1;
 				}
 			}
 		}
+		if (counter == len){
+			return 1;
+		}else{
+			return 0;
+		}
 	}
+}
+
+int main( void )
+{
+	unsigned int a[] = {};
+	unsigned int b[] = {};
+
+	if( scrambled(a,b,8) == 1 ){
+		printf("b same as a");
+	}else{
+		printf("b different");
+	}
+
 	return 0;
 }
+
