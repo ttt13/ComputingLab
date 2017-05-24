2e5b9d0f3bbd858ab320e9aac399d2e6ebe5b7aa
ttt13 <ttt13@sfu.ca>
2017-05-23 20:13:38 -0700

    identical is working

---
+++ 2/identical.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int identical( int a[], int b[], unsigned int len )
+{
+	if (len == 0){
+		return 1;
+	}
+	else {
+		for (int i = 0; i < len; i++){
+			if (a[i] != b[i]){
+				return 0;
+			}
+		}
+	return 1;
+	}
+}
