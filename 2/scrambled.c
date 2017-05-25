b8d42757e57c08cb63cf29f6d16ea7aa9d49c6a2
ttt13 <ttt13@sfu.ca>
2017-05-24 21:23:19 -0700

    scrambled is working

--- 2/scrambled.c
+++ 2/scrambled.c
@@ -2,25 +2,26 @@
 
 int scrambled( unsigned int a[], unsigned int b[], unsigned int len )
 {
-	unsigned int temp = 0;
 	int counter = 0;
-	if (len == 0){
+
+	if(len == 0)
+	{
 		return 1;
-	}else{
-		for (int i = 0; i < len; i++){
-			temp = a[i];
-			for (int n = 0; n < len; n++){
-				if (b[n] == temp){
-					counter += 1;
-				}
-			}
+	}
+	else
+	{
+		for(int i = 0; i < len; i++)
+		{
+			counter += (a[i] - b[i]);
 		}
-		if (counter == len){
+		if(counter == 0)
+		{
 			return 1;
-		}else{
+		}
+		else
+		{
 			return 0;
 		}
 	}
+	return 0;
 }
-
-
