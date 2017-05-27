a019ce9abae64b2b7f5b5d51a211c7d74c1bb5d5
ttt13 <ttt13@sfu.ca>
2017-05-26 21:10:33 -0700

    contains is working

--- 2/contains.c
+++ 2/contains.c
@@ -4,28 +4,34 @@
 
 int main( int argc, char* argv[] )
 {
+	if (argc < 3)
+	{
+		printf("false\n");
+		return 0;
+	}
+
 	int len1 = strlen(argv[1]);
 	int len2 = strlen(argv[2]);
 
 	char *str1 = argv[1];
 	char *str2 = argv[2];
 
-	if(len2 > len1)
+	if(len2 > len1 || argc < 3)
 	{
-		printf("False\n");
+		printf("false\n");
 		return 0;
 
 	}else{
-		for (int x = 0; x < (len1 - len2); x++)
+		for (int x = 0; x < (len1 - len2 + 1); x++)
 		{
 			int ret = strncmp(&str1[x], str2, len2);
-				if (ret == 0);
-				{
-					printf("True\n");
-					return 1;
-				}
+			//printf("%d\n", ret);
+
+			if (ret == 0){
+				printf("true\n");
+				return 0;
+			}
 		}
+	printf("false\n");
 	}
-	printf("False\n");
-	return 0;
 }
