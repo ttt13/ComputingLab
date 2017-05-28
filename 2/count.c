928f42dc526437270a36b71107134702fdb6616d
ttt13 <ttt13@sfu.ca>
2017-05-28 00:20:02 -0700

    count is working

--- 2/count.c
+++ 2/count.c
@@ -7,22 +7,36 @@ int main(void)
 	unsigned long int linecount = 0;
 
 	char c;
+	char last;
+
+	last = c = getchar();
+	if (c!= 32 && c!= 10 && c!= '\''){
+		charcount++;
+	}
+	if ( c != '\'' && c == '\n'){
+		linecount++;
+	}
 
 	while (c != EOF)
 	{
 		c = getchar();
 
-		if (c != ' ' && c != '\n' && c != '\''){
+		if (c != 32 && c != 10 && c != '\''){
 			charcount ++;
 // c!= 37 || 61<c<91 || 96<c<123
 		}
-		if (c != '\'' && (c == ' ' || c == '\n' || c == '-')){
-			wordcount++;
+		if ( (c == 32 || c == 45 || c == 10) )
+//&& (last == 46 || last == 44 || (64 < last < 91) || (96 < last < 123) ) ){
+// (c != '\'' && (c == ' ' || c == '\n' || c == '-')){
+		{
+			if (last == 46 || last == 44 || 64 < last < 91 || 96 < last < 123 ){
+				wordcount++;
+			}
 		}
 		if (c != '\'' && c  == '\n'){
 			linecount++;
 		}
-
+		last = c;
 	}
 	printf("%lu %lu %lu\n", charcount, wordcount, linecount);
 
