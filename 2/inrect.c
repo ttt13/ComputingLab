c7c4e1c2aa80b1fa1752c167c8c4f5fb9d158358
ttt13 <ttt13@sfu.ca>
2017-05-30 14:16:13 -0700

    inrect is working

--- 2/inrect.c
+++ 2/inrect.c
@@ -4,11 +4,9 @@ int InRectangle(float pt[2], float rect[4])
 {
 	if ((rect[0] > rect[2]) && (rect[1] > rect[3]) ){
 		if ( (pt[0] <= rect[0] && pt[0] >= rect[2]) && (pt[1] <= rect[1] && pt[1] >= rect[3]) ){
-			printf("True\n");
 			return 1;
 		}
 		else{
-			printf("False\n");
 			return 0;
 		}
 	}
@@ -17,11 +15,9 @@ int InRectangle(float pt[2], float rect[4])
 	else if ( (rect[0] < rect[2]) && (rect[1] < rect[3]) ){	
 		
 		if ( (pt[0] >= rect[0]) && (pt[0] <= rect[2]) && (pt[1] >= rect[1]) && (pt[1] <= rect[3]) ){
-			printf("True\n");
 			return 1;
 		}
 		else{
-			printf("False\n");
 			return 0;
 		}
 	}
