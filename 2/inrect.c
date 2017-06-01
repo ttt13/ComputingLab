1c18f77568b59770839ef509fc3ecafa038281f2
ttt13 <ttt13@sfu.ca>
2017-05-31 19:20:20 -0700

    task 10 edit

--- 2/inrect.c
+++ 2/inrect.c
@@ -15,7 +15,7 @@ int InRectangle(float pt[2], float rect[4])
 	} */
 
 	if ( ((pt[0] >= rect[0]) && (pt[0] <= rect[2])) || ( (pt[0] <= rect[0] && pt[0] >= rect[2]) ) ){
-		if ( ( (pt[1] >=rect[1]) && (pt[1] <= rect[3]) ) || ( (pt[1] <= rect[1]) && (pt[1] >= rect[2]) ) ){
+		if ( ( (pt[1] >=rect[1]) && (pt[1] <= rect[3]) ) || ( (pt[1] <= rect[1]) && (pt[1] >= rect[3]) ) ){
 			return 1;
 		}
 	}
